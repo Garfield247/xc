d7b00b8dade45f2b2de02acc7edf9c5708b546e3
Garfield247 <Garfield_lv@163.com>
2019-04-10 13:09:12 +0800

    del tmp

--- utils/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-    pc_mem = psutil.virtual_memory()
-    div_gb_factor = (1024.0 ** 3)
-    free = pc_mem.used/pc_mem.total
-
-    print("内存占用【%2f%】\n 剩余可用【%fGB】" %(free,float(pc_mem.available/div_gb_factor)))
