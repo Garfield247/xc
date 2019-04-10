307a9d5da4efa9394df42069f497cdccf990ae24
Garfield247 <Garfield_lv@163.com>
2019-04-10 11:31:24 +0800

    float >> int

--- crawl/dl_cmt.py
+++ crawl/dl_cmt.py
@@ -63,7 +63,7 @@ def dl_cmt_page(hotel):
     page = 1
     html = browser.page_source
     body = BeautifulSoup(html,"html.parser")
-    pages = int(re.findall(r'\((\d+)\)',body.find('span',{"id":"All_Comment"}).get_text())[0])/15+1
+    pages = int(int(re.findall(r'\((\d+)\)',body.find('span',{"id":"All_Comment"}).get_text())[0])/15+1)
     print(f'共{pages}页评论')
     while page<pages:
         html = browser.page_source
